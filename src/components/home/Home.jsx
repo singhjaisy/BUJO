57d94e6ccec21ca2a7a641ef1c9300b23d104355
singhjaisy <jaisysingh2001@gmail.com>
2024-06-23 02:44:10 +0530

    23-2

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import homecss from '../home/Home.module.css'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <section>
       <section className={homecss.sec1}>
           <h1>BUJO</h1>
       </section>
+      <section className={homecss.sec2}>
+        <h1>start adding tasks</h1>
+        <button className={homecss.sec2btn}><Link to="/components/Tasks">add tasks</Link></button>
+      </section>
+      <section className={homecss.sec3}>
+
+      </section>
+      <footer>  </footer>
     </section>
   )
 }
-
 export default Home
