879a1fbfc08bee0d6c27bea29ca8b425a04b36c9
singhjaisy <jaisysingh2001@gmail.com>
2024-06-23 00:10:34 +0530

    first

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import homecss from '../home/Home.module.css'
 const Home = () => {
   return (
-    <div>
-        <h1 className={homecss.heading}>Home</h1>
-        <p>This is the home page</p>
-    </div>
+    <section>
+      <section className={homecss.sec1}>
+          
+      </section>
+    </section>
   )
 }
 
