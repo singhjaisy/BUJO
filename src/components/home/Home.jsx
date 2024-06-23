4bc557dc0b1df568cf458fce00b4f406dfd982ba
singhjaisy <jaisysingh2001@gmail.com>
2024-06-23 12:09:38 +0530

    23-3

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,59 @@
-import React from 'react'
-import homecss from '../home/Home.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import homecss from "../home/Home.module.css";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 const Home = () => {
   return (
     <section>
       <section className={homecss.sec1}>
-          <h1>BUJO</h1>
+        <h1>BUJO</h1>
       </section>
       <section className={homecss.sec2}>
         <h1>start adding tasks</h1>
-        <button className={homecss.sec2btn}><Link to="/components/Tasks">add tasks</Link></button>
+        <button className={homecss.sec2btn}>
+          <Link to="/components/Tasks">add tasks</Link>
+        </button>
       </section>
       <section className={homecss.sec3}>
-
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
       </section>
-      <footer>  </footer>
+      <footer> </footer>
     </section>
-  )
-}
-export default Home
+  );
+};
+export default Home;
