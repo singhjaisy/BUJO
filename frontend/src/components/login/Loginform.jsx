c9779122491665804e946fa97d283a4e8374035e
parul124 <sharmaparu48@gmail.com>
2024-07-17 21:29:35 +0530

    login complete

--- frontend/src/components/login/Loginform.jsx
+++ frontend/src/components/login/Loginform.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Loginform = () => {
   return (
     <div>
       <form action=""> 
-        <label htmlFor="">Name</label>
+        <label htmlFor="">UserName </label>
         <input type="text" placeholder='Enter Your Name' />
+        <label htmlFor="">Password</label>
+        <input type="text" placeholder='Enter Your Password' />
+        <button type='submit'> Login</button>
+
+        <aside>Don't have a Account <Link to="/signup">SignUp</Link></aside>
       </form>
       </div>
   )
